3a15cb0126cc194c2d821ee310de2f4cb5470f74
Roger Kommer <r.kommer.extern@micromata.de>
2016-04-14 12:19:07 +0200

    GENOME-1716. Copyright header

--- tpsb-common/src/test/java/de/micromata/genome/tpsb/executor/GroovyShellExecutorTest.java
+++ tpsb-common/src/test/java/de/micromata/genome/tpsb/executor/GroovyShellExecutorTest.java
@@ -1,3 +1,19 @@
+//
+// Copyright (C) 2010-2016 Micromata GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 package de.micromata.genome.tpsb.executor;
 
 import org.junit.Test;
@@ -15,26 +31,26 @@ public class GroovyShellExecutorTest
         + "import org.junit.Test;\r\n"
         + "\r\n"
         + "/**\r\n"
-        + " * Tested die autoamatische Zuwahl von Sperrgut\r\n"
+        + " * A Test\r\n"
         + " *\r\n"
         + " * @author Roger Rene Kommer (r.kommer.extern@micromata.de)\r\n"
         + " */\r\n"
         + "@TpsbTestSuite\r\n"
-        + "public class ProductServiceAutoSperrTest\r\n"
+        + "public class ProductServiceTest\r\n"
         + "{\r\n"
         + "  /**\r\n"
-        + "   * Sendung, deren Ausmasse Sperrgut benötigt.\r\n"
+        + "   * SampleDoc \r\n"
         + "   *\r\n"
         + "   */\r\n"
         + "  @Test\r\n"
         + "  public void testAutselect()\r\n"
         + "  {\r\n"
-        + "   System.out.println(\"TPSBSTARTMETHOD: com.dpdhl.vls.productmodel.validation.ProductServiceAutoSperrTest.testAutselect\");\r\n"
+        + "   System.out.println(\"TPSBSTARTMETHOD: ProductServiceTest.testAutselect\");\r\n"
         + "    StartTestBuilder commonTestBuilder = new StartTestBuilder();\r\n"
         + "   System.out.println(\"TPSBTESTSTEPNUM: 1\");\r\n"
         + "    ;\r\n"
         + " //if (true) throw new RuntimeException(\"oops\");\n"
-        + "   System.out.println(\"TPSBENDMETHOD: com.dpdhl.vls.productmodel.validation.ProductServiceAutoSperrTest.testAutselect\");\r\n"
+        + "   System.out.println(\"TPSBENDMETHOD: ProductServiceTest.testAutselect\");\r\n"
         + "  }\r\n"
         + "}";
     try {
--- tpsb-htmlunit/src/main/java/de/micromata/genome/tpsb/htmlunit/HtmlWebClient.java
+++ tpsb-htmlunit/src/main/java/de/micromata/genome/tpsb/htmlunit/HtmlWebClient.java
@@ -1,12 +1,19 @@
-/////////////////////////////////////////////////////////////////////////////
 //
-// Project   DHL-ParcelOnlinePostage
+// Copyright (C) 2010-2016 Micromata GmbH
 //
-// Author    roger@micromata.de
-// Created   16.09.2008
-// Copyright Micromata 16.09.2008
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-/////////////////////////////////////////////////////////////////////////////
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 package de.micromata.genome.tpsb.htmlunit;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
@@ -23,15 +30,6 @@ public class HtmlWebClient extends WebClient
 
   private static final long serialVersionUID = 7341210499923247162L;
 
-  // public static String defaultHost = "http://localhost:8080";
-  //
-  // public static String defaultAppDir = "/genome";
-  //
-  // public static String defaultBaseUrl = defaultHost + defaultAppDir;
-
-  // public static String overwriteUrl = null; // "https://secure.micromata.de/popweb";
-
-  // static String overwriteUrl = "http://localhost:8080/popweb";//"https://secure.micromata.de/popweb";
 
   public String baseUrl;
 
